914d5d0ccfe5ced378dd91416363e8c12a0cbb77
Guillermo-alt <guillermo76ov@hotmail.com>
2022-03-24 02:05:23 -0600

    update nav menu, acerca, herramientas

--- php/connect.php
+++ php/connect.php
@@ -1,17 +1,24 @@
 <?php
 
 //$host='localhost';
+
+$host='localhost';
+$user='root';
+$pass='';
+$dataBase='u689962032_LensLab';
+
+/* colocar esto en un .env
 $host='lenslab-uamc.net';
 $user='u689962032_Guillermo';
 $pass='Kinomoto76';
-$dataBase='u689962032_LensLab';
+$dataBase='u689962032_LensLab';*/
 
 // Create connection
 $conn = new mysqli($host, $user, $pass,$dataBase, 3306);
 
 // Check connection
 if ($conn->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
+    echo 'Fallo al conectar a MySQL: (" . $conn->connect_errno . ") ' . $conn->connect_error;
 }
 //echo $conn->host_info . "\n";
 
